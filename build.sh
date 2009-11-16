50f251d215547f85c897e047ee8b43b869b48dda
Manojav Sridhar <manojav@manojav.com>
2009-11-16 10:44:08 -0500

    fixed sound and also systeinfo screen

--- build.sh
+++ build.sh
@@ -159,7 +159,9 @@ then
 	echo "<!-- $SKINNAME skin revision: ${REVISION} - built with build.sh version 0.1 -->" > $DIRNAME/BUILD/$SKINNAME/${DEFAULTRESWIDE}/revision.xml
 	echo "<includes>" >> $DIRNAME/BUILD/$SKINNAME/${DEFAULTRESWIDE}/revision.xml
 	echo "<include name=\"Revision\">" >> $DIRNAME/BUILD/$SKINNAME/${DEFAULTRESWIDE}/revision.xml
-	echo "<label>$SKINNAME ${VERSION}, SVN - r${REVISION}</label>" >> $DIRNAME/BUILD/$SKINNAME/${DEFAULTRESWIDE}/revision.xml
+ 	# dont use svn any more git's version is way cooler	
+	# echo "<label>$SKINNAME ${VERSION}, SVN - r${REVISION}</label>" >> $DIRNAME/BUILD/$SKINNAME/${DEFAULTRES}/revision.xml
+	echo "<label>$SKINNAME ${VERSION}</label>" >> $DIRNAME/BUILD/$SKINNAME/${DEFAULTRES}/revision.xml
 	echo "</include>" >> $DIRNAME/BUILD/$SKINNAME/${DEFAULTRESWIDE}/revision.xml
 	echo "</includes>" >> $DIRNAME/BUILD/$SKINNAME/${DEFAULTRESWIDE}/revision.xml
 	echo "done."
